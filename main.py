466e3060137331eca271e576233d585823a035c4
Abhishek S <67963799+iamA333@users.noreply.github.com>
2023-11-27 01:10:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,11 +30,6 @@ print(classNames)
 # cap = cv2.VideoCapture(0)
 cap = webrtc_streamer(
     key="object-detection",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration={
-        "iceServers": get_ice_servers(),
-        "iceTransportPolicy": "relay",
-    },
     video_frame_callback=video_frame_callback,
     media_stream_constraints={"video": True, "audio": False},
     async_processing=True,
